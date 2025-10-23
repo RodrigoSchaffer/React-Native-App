a8d5bdfb2857af1c4eed8cc09a4093a69ea4954b
rodrigoschaffer <rodrigoschafferc@gmail.com>
2025-10-22 23:58:19 -0300

    Flatlist + internet connection

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import styles from '../Styles';
 import { useUser } from '../UserContext';
 
 
 const HomeScreen = () => {
+    const notes = ([
+    { id: '1', text: 'Comprar Pão' },
+    { id: '2', text: 'Estudar' },
+    { id: '3', text: 'Fazer exercício' },
+    { id: '4', text: 'Ir à Igreja' },
+    { id: '5', text: 'Ir à Faculdade' },
+  ]);
+ 
+    const renderItem = ({ item }) => (
+    <View style={notestyle.item}>
+      <Text style={notestyle.text}>{item.text}</Text>
+    </View>
+  );
     const { user } = useUser();
   return (
     <View style={styles.container}>
@@ -12,15 +25,44 @@ const HomeScreen = () => {
       <View style={{ marginTop: 20 }}>
         {user ? (
           <>
-            <Text style={{color: "#61764B", fontSize: 15, marginTop: -15}}>Welcome {user.name}. </Text>
+            <Text style={{color: "#61764B", fontSize: 20, marginTop: -15}}>Welcome {user.name}. </Text>
           </>
         ) : (
           <Text style={styles.header}></Text>
         )}
+        <View style={notestyle.container}>
+        <FlatList
+        data={notes}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        style={notestyle.list}
         
+      />
+        </View>
+
       </View>
     </View>
   ); 
 };
+const notestyle = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
+  list: {
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#f0f0f0',
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#61764B',
+  },
+});
 
 export default HomeScreen;
--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
-import { useUser } from '../UserContext'; 
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import * as Network from "expo-network";
+import { useUser } from "../UserContext";
 
 const LoginScreen = ({ navigation }) => {
   const { signIn } = useUser();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    if (email === 'abc@gmail.com' && password === '123') {
-      const userData = { name: 'Abc', email }; 
+    const networkState = await Network.getNetworkStateAsync();
+    if (!networkState.isConnected) {
+      alert('No internet connection!');
+      return;
+    }
+    if (email === "abc@gmail.com" && password === "123") {
+      const userData = { name: "Abc", email };
       await signIn(userData);
-      navigation.replace("Main"); 
+      navigation.replace("Main");
     } else {
-      alert('Username or email invalid');
+      alert("Username or email invalid");
     }
   };
 
@@ -34,10 +47,14 @@ const LoginScreen = ({ navigation }) => {
         onChangeText={setPassword}
       />
       <View style={styles.buttonContainer}>
-      <TouchableOpacity style={styles.button} onPress={handleLogin} color="#61764B">
-        <Text style={styles.buttonText}> Login </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleLogin}
+          color="#61764B"
+        >
+          <Text style={styles.buttonText}> Login </Text>
         </TouchableOpacity>
-        </View>
+      </View>
     </View>
   );
 };
@@ -45,45 +62,45 @@ const LoginScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
     padding: 16,
   },
   header: {
     fontSize: 32,
-    fontWeight: 'bold',
-    color: '#61764B',
+    fontWeight: "bold",
+    color: "#61764B",
     marginBottom: 20,
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 12,
     paddingLeft: 8,
     borderRadius: 4,
-    width: '80%',
+    width: "80%",
   },
   buttonContainer: {
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    width: '100%',
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "100%",
     maxWidth: 300,
   },
   button: {
-    backgroundColor: '#61764B',
+    backgroundColor: "#61764B",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 8,
     marginBottom: 20, // Space between the buttons
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 16,
   },
 });
