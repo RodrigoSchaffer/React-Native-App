e9f2b79d5b6a4cb3117f73ff6aa8480d0396a51f
rodrigoschaffer <rodrigoschafferc@gmail.com>
2025-12-12 11:48:11 -0300

    firebase

---
+++ firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGTfTquMeK-g86aqVdQo2SH3riXQHS0fg",
+  authDomain: "react-native-app-9798a.firebaseapp.com",
+  projectId: "react-native-app-9798a",
+  storageBucket: "react-native-app-9798a.firebasestorage.app",
+  messagingSenderId: "183285284914",
+  appId: "1:183285284914:web:eb3d1a20542a6fdb667c2b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
